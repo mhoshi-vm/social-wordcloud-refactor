af983e934aca30948ad9c561c1b58c4d9bbb7d02
mhoshi-vm <machi.hoshino@broadcom.com>
2026-01-05 23:43:08 +0900

    Update for timescaledb

--- social-analytics/src/main/resources/db/postgres/updateGisInfo_2.sql
+++ social-analytics/src/main/resources/db/postgres/updateGisInfo_2.sql
@@ -1,3 +1,2 @@
 INSERT INTO gis_info (message_id, msg_timestamp, srid, gis, reason)
-VALUES (?, ?, ?, ?, ?) ON CONFLICT (message_id) DO
-UPDATE SET msg_timestamp = EXCLUDED.msg_timestamp, gis = EXCLUDED.gis, reason = EXCLUDED.reason;
+VALUES (?, ?, ?, ?, ?)
